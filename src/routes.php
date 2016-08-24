d636a5861feddb59a28e80ee3839f1662721115b
Pablo M. Rivas <pmrivas@gmail.com>
2016-08-24 14:08:24 -0300

    Suppress vendors, touched readme.md

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,18 @@
 <?php
 // Routes
 
+/*
 $app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});*/
+$app->get('/init', function ($request, $response, $args) { //Get init info...
+  $this->logger->info("yadbapi init");
+	//$id=$args['id']*1;
+	$initdata=new stdClass;
+	$initdata->user="Test User";
+  return $response->withJson($initdata);
 });
